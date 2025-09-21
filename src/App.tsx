5fc3364f1d2ec2a07b9299b6d5b3b40b462700ec
Noah <coder.noah@gmail.com>
2025-09-21 23:49:29 +0900

    chakra ui 추가, 기본 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,100 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import * as ui from '@chakra-ui/react'
 import './App.css'
 import { Map } from './Map'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div style={{
-        width: '100dvw'
-      }}>
-        <Map />
-      </div>
-    </>
+    <ui.Flex w='100dvw' h='100dvh'
+      justifyContent={'center'}
+    >
+      <ui.Box w='80dvw'>
+        <ui.VStack overflowY={'auto'} gap={15}>
+          <ui.Flex justifyItems={'center'} w='full' h='15dvh'
+            alignItems={'center'}
+            justifyContent={'center'}
+          >
+            {/* Title */}
+            <ui.Text>//TODO::타이틀(사랑하는 어머니, 심순옥 여사님의 70번째 생신을 축하해 주세요.)</ui.Text>
+          </ui.Flex>
+          <ui.Flex justifyItems={'center'} w='full' h='80dvh'
+            alignItems={'center'}
+            justifyContent={'center'}
+            borderWidth={1}
+            borderColor={'white'}
+          >
+            {/* Intro */}
+            <ui.Text>//TODO::인트로 (맨트 + 가족 사진?)</ui.Text>
+          </ui.Flex>
+          <ui.Flex w='full' h='20dvh'
+            justifyItems={'center'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            borderWidth={1}
+            borderColor={'white'}
+          >
+            {/* 연락처 */}
+            <ui.Grid
+              w='full' h='full'
+              templateAreas={`
+            'title title'
+            'father mother'
+            'misa mingu'
+            `}
+              templateRows={'50px 1fr 1fr'}
+              templateColumns={'1fr 1fr'}
+              gap={1}
+              justifyItems={'center'}
+              alignItems={'center'}
+            >
+              <ui.GridItem area='title'>
+                <ui.Text>//TODO::연락처</ui.Text>
+              </ui.GridItem>
+              <ui.GridItem area='father'>
+                <ui.Flex>
+                  <ui.Text>남편:김준일</ui.Text>
+                </ui.Flex>
+              </ui.GridItem>
+              <ui.GridItem area='mother'>
+                <ui.Flex>
+                  <ui.Text>심순옥</ui.Text>
+                </ui.Flex>
+              </ui.GridItem>
+              <ui.GridItem area='misa'>
+                <ui.Flex>
+                  <ui.Text>장녀:김미사</ui.Text>
+                </ui.Flex>
+              </ui.GridItem>
+              <ui.GridItem area='mingu'>
+                <ui.Flex>
+                  <ui.Text>장남:김민구</ui.Text>
+                </ui.Flex>
+              </ui.GridItem>
+            </ui.Grid>
+          </ui.Flex>
+          <ui.Flex justifyItems={'center'} w='full' h='80dvh'
+            alignItems={'center'}
+            justifyContent={'center'}
+            borderWidth={1}
+            borderColor={'white'}
+          >
+            {/* 갤러리 */}
+            <ui.Text>//TODO::갤러리 (엄마, 아빠 사진?)</ui.Text>
+          </ui.Flex>
+
+          <ui.Flex justifyItems={'center'} w='full' h='10dvh'
+            alignItems={'center'}
+            justifyContent={'center'}
+            borderWidth={1}
+            borderColor={'white'}
+          >
+            {/* 오시는 길 */}
+            <ui.Text>//TODO::오시는 길</ui.Text>
+          </ui.Flex>
+          <Map />
+        </ui.VStack>
+      </ui.Box>
+    </ui.Flex>
   )
 }
 
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,51 +1,56 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
 export const Map = () => {
   const bestFit = { w: 640, h: 392 };
   const aspectRatio = bestFit.w / bestFit.h;
-  const dw = window.innerWidth < bestFit.w ? window.innerWidth * 0.8 : bestFit.w;
+  // const dw = window.innerWidth < bestFit.w ? window.innerWidth * 0.8 : bestFit.w;
+  const dw = React.useMemo(() => {
+    return window.innerWidth * 0.8;
+  }, [window.innerWidth]);
   const dh = dw / aspectRatio;
 
   return (
-    <>
+    <Box style={{
+      font: 'normal normal 400 12px/normal dotum, sans-serif',
+      width: `${dw}px`,
+      height: `${dh}px`,
+      color: '#333',
+      position: 'relative',
+    }}>
+      {/* <div style={{ height: `${dh}px` }}> */}
+      <div>
+        <a href="https://map.kakao.com/?urlX=501339.0000000007&amp;urlY=1073443.0000000005&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;srcid=1407060805&amp;map_type=TYPE_MAP&amp;from=roughmap" target="_blank">
+          <img className="map" src="http://t1.daumcdn.net/roughmap/imgmap/70e63490514236a8c30c6c3e76d80c5bf332d7bd7cd7bb81b65ae4d295cbec92" width={`${dw - 2}px`} height={`${dh - 2}px`}
+            style={{ border: '1px solid #ccc' }} />
+        </a>
+      </div>
       <div style={{
-        font: 'normal normal 400 12px/normal dotum, sans-serif',
-        width: `${dw}px`,
-        height: `${dh}px`,
-        color: '#333',
-        position: 'relative',
+        overflow: 'hidden',
+        padding: '7px 11px',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        borderRadius: '0px 0px 2px 2px',
+        backgroundColor: 'rgb(249, 249, 249)',
       }}>
-        <div style={{ height: `${dh}px` }}>
-          <a href="https://map.kakao.com/?urlX=501339.0000000007&amp;urlY=1073443.0000000005&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;srcid=1407060805&amp;map_type=TYPE_MAP&amp;from=roughmap" target="_blank">
-            <img className="map" src="http://t1.daumcdn.net/roughmap/imgmap/70e63490514236a8c30c6c3e76d80c5bf332d7bd7cd7bb81b65ae4d295cbec92" width={`${dw - 2}px`} height={`${dh - 2}px`}
-              style={{ border: '1px solid #ccc' }} />
-          </a>
-        </div>
-        <div style={{
-          overflow: 'hidden',
-          padding: '7px 11px',
-          border: '1px solid rgba(0, 0, 0, 0.1)',
-          borderRadius: '0px 0px 2px 2px',
-          backgroundColor: 'rgb(249, 249, 249)',
-        }}>
-          <a href="https://map.kakao.com" target="_blank" style={{ float: 'left' }}>
+        {/* <a href="https://map.kakao.com" target="_blank" style={{ float: 'left' }}>
             <img src="//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png" width="72" height="16" alt="카카오맵" style={{ display: 'block', width: '72px', height: '16px' }} />
-          </a>
-          <div style={{ float: 'right', position: 'relative', top: '1px', fontSize: '11px' }}>
-            <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;srcid=1407060805&amp;confirmid=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;rv=on" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>로드뷰</a>
-            <span style={{ width: '1px', padding: '0', margin: '0 8px 0 9px', height: '11px', verticalAlign: 'top', position: 'relative', top: '2px', borderLeft: '1px solid #d0d0d0', float: 'left' }}></span>
-            {/* <span style="width: 1px;padding: 0;margin: 0 8px 0 9px;height: 11px;vertical-align: top;position: relative;top: 2px;border-left: 1px solid #d0d0d0;float: left;"></span> */}
-            {/* <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;eX=501339.0000000007&amp;eY=1073443.0000000005" style="float:left;height:15px;padding-top:1px;line-height:15px;color:#000;text-decoration: none;">길찾기</a> */}
-            <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;eX=501339.0000000007&amp;eY=1073443.0000000005" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>길찾기</a>
-            <span style={{ width: '1px', padding: '0', margin: '0 8px 0 9px', height: '11px', verticalAlign: 'top', position: 'relative', top: '2px', borderLeft: '1px solid #d0d0d0', float: 'left' }}></span>
-            {/* <span style="width: 1px;padding: 0;margin: 0 8px 0 9px;height: 11px;vertical-align: top;position: relative;top: 2px;border-left: 1px solid #d0d0d0;float: left;"></span> */}
-            {/* <a target="_blank" href="https://map.kakao.com?map_type=TYPE_MAP&amp;from=roughmap&amp;srcid=1407060805&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;urlX=501339.0000000007&amp;urlY=1073443.0000000005" style="float:left;height:15px;padding-top:1px;line-height:15px;color:#000;text-decoration: none;">지도 크게 보기</a> */}
-            <a target="_blank" href="https://map.kakao.com?map_type=TYPE_MAP&amp;from=roughmap&amp;srcid=1407060805&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;urlX=501339.0000000007&amp;urlY=1073443.0000000005" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>지도 크게 보기</a>
-          </div>
+          </a> */}
+        <div style={{ float: 'right', position: 'relative', top: '1px', fontSize: '11px' }}>
+          {/* <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;srcid=1407060805&amp;confirmid=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;rv=on" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>로드뷰</a>
+            <span style={{ width: '1px', padding: '0', margin: '0 8px 0 9px', height: '11px', verticalAlign: 'top', position: 'relative', top: '2px', borderLeft: '1px solid #d0d0d0', float: 'left' }}></span> */}
+          {/* <span style="width: 1px;padding: 0;margin: 0 8px 0 9px;height: 11px;vertical-align: top;position: relative;top: 2px;border-left: 1px solid #d0d0d0;float: left;"></span> */}
+          {/* <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;eX=501339.0000000007&amp;eY=1073443.0000000005" style="float:left;height:15px;padding-top:1px;line-height:15px;color:#000;text-decoration: none;">길찾기</a> */}
+          <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;eX=501339.0000000007&amp;eY=1073443.0000000005" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>길찾기</a>
+          <span style={{ width: '1px', padding: '0', margin: '0 8px 0 9px', height: '11px', verticalAlign: 'top', position: 'relative', top: '2px', borderLeft: '1px solid #d0d0d0', float: 'left' }}></span>
+          {/* <span style="width: 1px;padding: 0;margin: 0 8px 0 9px;height: 11px;vertical-align: top;position: relative;top: 2px;border-left: 1px solid #d0d0d0;float: left;"></span> */}
+          {/* <a target="_blank" href="https://map.kakao.com?map_type=TYPE_MAP&amp;from=roughmap&amp;srcid=1407060805&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;urlX=501339.0000000007&amp;urlY=1073443.0000000005" style="float:left;height:15px;padding-top:1px;line-height:15px;color:#000;text-decoration: none;">지도 크게 보기</a> */}
+          <a target="_blank" href="https://map.kakao.com?map_type=TYPE_MAP&amp;from=roughmap&amp;srcid=1407060805&amp;itemId=1407060805&amp;q=%EB%B0%B1%EC%9A%B4%EC%9B%90&amp;urlX=501339.0000000007&amp;urlY=1073443.0000000005" style={{ float: 'left', height: '15px', paddingTop: '1px', lineHeight: '15px', color: '#000', textDecoration: 'none' }}>지도 크게 보기</a>
         </div>
-        <div>
-          <span style={{ borderBottom: '0px none #333333', position: 'absolute', left: '-25px', top: '-136px', width: '0px', height: '40px' }}>
-          </span>
-        </div>
-      </div >
-    </>
+      </div>
+      <div>
+        <span style={{ borderBottom: '0px none #333333', position: 'absolute', left: '-25px', top: '-136px', width: '0px', height: '40px' }}>
+        </span>
+      </div>
+    </Box>
   )
 }
